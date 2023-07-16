7f64c953a80226e3b32bb9bcff0bfbe01aac40ee
刘源昭 <2945708184@qq.com>
2023-07-16 16:31:38 +0800

    使用Redis用来缓存

--- src/main/java/com/example/ruijidemo/controller/DishController.java
+++ src/main/java/com/example/ruijidemo/controller/DishController.java
@@ -13,9 +13,11 @@ import com.example.ruijidemo.service.DishService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 @RestController
@@ -30,6 +32,9 @@ public class DishController {
 
     @Autowired
     private CategoryService categoryService;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     /**
      * 新增菜品
@@ -47,36 +52,36 @@ public class DishController {
     }
 
     @GetMapping("/page")
-    public R<Page> page(int page,int pageSize,String name){
+    public R<Page> page(int page, int pageSize, String name) {
 
-        Page<Dish> pageInfo = new Page<>(page,pageSize);
+        Page<Dish> pageInfo = new Page<>(page, pageSize);
         Page<DishDto> dishDtoPage = new Page<>();
 
         //条件构造器
         LambdaQueryWrapper<Dish> queryWrapper = new LambdaQueryWrapper<>();
         //添加过滤条件
-        queryWrapper.like(name != null,Dish::getName,name);
+        queryWrapper.like(name != null, Dish::getName, name);
         //添加排序条件
         queryWrapper.orderByDesc(Dish::getUpdateTime);
 
         //执行分页查询
-        dishService.page(pageInfo,queryWrapper);
+        dishService.page(pageInfo, queryWrapper);
 
         //对象拷贝
-        BeanUtils.copyProperties(pageInfo,dishDtoPage,"records");
+        BeanUtils.copyProperties(pageInfo, dishDtoPage, "records");
 
         List<Dish> records = pageInfo.getRecords();
 
         List<DishDto> list = records.stream().map((item) -> {
             DishDto dishDto = new DishDto();
 
-            BeanUtils.copyProperties(item,dishDto);
+            BeanUtils.copyProperties(item, dishDto);
 
             Long categoryId = item.getCategoryId();//分类id
             //根据id查询分类对象
             Category category = categoryService.getById(categoryId);
 
-            if(category != null){
+            if (category != null) {
                 String categoryName = category.getName();
                 dishDto.setCategoryName(categoryName);
             }
@@ -91,12 +96,13 @@ public class DishController {
 
     /**
      * 根据id查询菜品信息
+     *
      * @param id
      * @return
      */
 
     @GetMapping("/{id}")
-    public R<DishDto> get(@PathVariable Long id){
+    public R<DishDto> get(@PathVariable Long id) {
 
         DishDto dishDto = dishService.getByIdWithFlavor(id);
 
@@ -106,13 +112,19 @@ public class DishController {
     @PutMapping
     public R<String> update(@RequestBody DishDto dishDto) {
         log.info(dishDto.toString());
+
         dishService.updateWithFlavor(dishDto);
+     //清理某个分类下面的菜品缓存数据
+        String key = "dish_" + dishDto.getCategoryId() + "_" + dishDto.getStatus();
+        redisTemplate.delete(key);
+
+
 
         return R.success("成功");
     }
 
 
-//    @GetMapping("/list")
+    //    @GetMapping("/list")
 //    public R<List<Dish>> list(Dish dish){
 //        //构造条件
 //        LambdaQueryWrapper<Dish> lambdaQueryWrapper=new LambdaQueryWrapper<>();
@@ -126,44 +138,59 @@ public class DishController {
 //        return R.success(list);
 //
 //    }
-@GetMapping("/list")
-public R<List<DishDto>> list(Dish dish) {
+    @GetMapping("/list")
+    public R<List<DishDto>> list(Dish dish) {
 
-    //构造查询条件
-    LambdaQueryWrapper<Dish> lambdaQueryWrapper = new LambdaQueryWrapper<>();
-    //添加条件，查询状态为1的（起售状态）
-    lambdaQueryWrapper.eq(Dish::getStatus, 1);
-    lambdaQueryWrapper.eq(dish.getCategoryId() != null, Dish::getCategoryId, dish.getCategoryId());
-    //条件排序条件
-    lambdaQueryWrapper.orderByAsc(Dish::getSort).orderByDesc(Dish::getUpdateTime);
-
-    List<Dish> list = dishService.list(lambdaQueryWrapper);
-
-    List<DishDto> dishDtoList = list.stream().map((item) -> {
-        DishDto dishDto = new DishDto();
-
-        BeanUtils.copyProperties(item, dishDto);
-        Long categoryId = item.getCategoryId();
-        //根据id查分类对象
-        Category category = categoryService.getById(categoryId);
-        if (category != null) {
-            String categoryName = category.getName();
-            dishDto.setCategoryName(categoryName);
+        List<DishDto> dishDtoList = null;
+        //动态构造Key
+        String key = "dish_" + dish.getCategoryId() + "_" + dish.getStatus();
+        //先从redis中获取缓存数据
+        dishDtoList = (List<DishDto>) redisTemplate.opsForValue().get(key);
+        if (dishDtoList != null) {
+            //如果存在，则直接返回，无需查询数据库
+            return R.success(dishDtoList);
         }
 
-        //当前菜品id
-        Long dishId = item.getId();
-        LambdaQueryWrapper<DishFlavor> queryWrapper = new LambdaQueryWrapper<>();
-        queryWrapper.eq(DishFlavor::getDishId, dishId);
-        //SQL: select* from dishflavor where dish_id=?;
-        List<DishFlavor> dishFlavorlist = dishFlavorService.list(queryWrapper);
-        dishDto.setFlavors(dishFlavorlist);
-        return dishDto;
-    }).collect(Collectors.toList());
+        //如果存在直接返回，否在查询存入redis
 
-    return R.success(dishDtoList);
-}
+        //构造查询条件
+        LambdaQueryWrapper<Dish> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+        //添加条件，查询状态为1的（起售状态）
+        lambdaQueryWrapper.eq(Dish::getStatus, 1);
+        lambdaQueryWrapper.eq(dish.getCategoryId() != null, Dish::getCategoryId, dish.getCategoryId());
+        //条件排序条件
+        lambdaQueryWrapper.orderByAsc(Dish::getSort).orderByDesc(Dish::getUpdateTime);
 
+        List<Dish> list = dishService.list(lambdaQueryWrapper);
+
+        dishDtoList = list.stream().map((item) -> {
+            DishDto dishDto = new DishDto();
+
+            BeanUtils.copyProperties(item, dishDto);
+            Long categoryId = item.getCategoryId();
+            //根据id查分类对象
+            Category category = categoryService.getById(categoryId);
+            if (category != null) {
+                String categoryName = category.getName();
+                dishDto.setCategoryName(categoryName);
+            }
+
+            //当前菜品id
+            Long dishId = item.getId();
+            LambdaQueryWrapper<DishFlavor> queryWrapper = new LambdaQueryWrapper<>();
+            queryWrapper.eq(DishFlavor::getDishId, dishId);
+            //SQL: select* from dishflavor where dish_id=?;
+            List<DishFlavor> dishFlavorlist = dishFlavorService.list(queryWrapper);
+            dishDto.setFlavors(dishFlavorlist);
+            return dishDto;
+        }).collect(Collectors.toList());
+
+
+        //如果不存在
+        redisTemplate.opsForValue().set(key, dishDtoList, 60, TimeUnit.MINUTES);
+
+        return R.success(dishDtoList);
+    }
 
 
 }
--- src/main/java/com/example/ruijidemo/controller/UserController.java
+++ src/main/java/com/example/ruijidemo/controller/UserController.java
@@ -8,6 +8,7 @@ import com.example.ruijidemo.utils.ValidateCodeUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/user")
@@ -25,6 +27,9 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
 
 
     @PostMapping("/sendMsg")
@@ -39,7 +44,9 @@ public class UserController {
             //调用阿里云api
 //            SMSUtils.sendMessage("","",phone,code);
             //保存session
-            session.setAttribute(phone, code);
+//            session.setAttribute(phone, code);
+
+            stringRedisTemplate.opsForValue().set(phone,code,5, TimeUnit.MINUTES);
             return R.success("验证码发送成功");
 
         }
@@ -56,7 +63,10 @@ public class UserController {
         //获取验证码
         String code = map.get("code").toString();
         //从Session中获取保存的验证码
-        Object codeInSession = session.getAttribute(phone);
+//        Object codeInSession = session.getAttribute(phone);
+
+        //从Redis中取出验证码
+        String codeInSession = stringRedisTemplate.opsForValue().get(phone);
         //进行验证码比对（页面提交的验证码和Session中保存的验证码比对）
         if (codeInSession != null && codeInSession.equals(code)) {
             //如果能够比对成功，说明登录成功
@@ -72,6 +82,12 @@ public class UserController {
                 userService.save(user);
             }
             session.setAttribute("user", user.getId());
+
+
+            //成功之后直接删除redis中的数据
+            stringRedisTemplate.delete(phone);
+
+
             return R.success(user);
         }
         return R.error("登录失败");
