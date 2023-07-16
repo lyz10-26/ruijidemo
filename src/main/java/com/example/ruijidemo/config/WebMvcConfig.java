27767cd3bc4465ea860cca74ff78f23322b372ab
刘源昭 <2945708184@qq.com>
2023-07-16 22:51:28 +0800

    加入swagger

--- src/main/java/com/example/ruijidemo/config/WebMvcConfig.java
+++ src/main/java/com/example/ruijidemo/config/WebMvcConfig.java
@@ -1,25 +1,40 @@
 package com.example.ruijidemo.config;
 
 import com.example.ruijidemo.common.JacksonObjectMapper;
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.List;
 
 @Configuration
 @Slf4j
+@EnableSwagger2
+@EnableKnife4j
 public class WebMvcConfig extends WebMvcConfigurationSupport {
 
     @Override
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
         log.info("开始进行静态资源映射...");
+
         //前端地址映射地址--------后端文件地址
         registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/static/backend/");
         registry.addResourceHandler("/front/**").addResourceLocations("classpath:/static/front/");
+        registry.addResourceHandler("doc.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+
     }
     @Override
     protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
@@ -30,7 +45,22 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
         //将转换器追加到mvc框架中
         converters.add(0,messageConverter);
 
-
-
+    }
+    @Bean
+    public Docket createRestApi() {
+        //文档类型
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.ruijidemo.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("瑞吉外卖项目")
+                .version("1.0")
+                .description("瑞吉外卖接口文档")
+                .build();
     }
 }
